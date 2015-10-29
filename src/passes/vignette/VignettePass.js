026026254e34265e01080ffd6cc33c2392d5391c
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2015-10-29 14:55:24 +0100

    added vignette pass

---
+++ src/passes/vignette/VignettePass.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var glslify = require('glslify');
+var Pass = require('../../Pass');
+var vertex = glslify('../../shaders/vertex/basic.glsl');
+var fragment = glslify('./vignette-fs.glsl');
+
+function VignettePass(boost, reduction) {
+  Pass.call(this);
+
+  this.setShader(vertex, fragment);
+
+  this.params.boost = boost || 2;
+  this.params.reduction = reduction || 2;
+}
+
+module.exports = VignettePass;
+
+VignettePass.prototype = Object.create(Pass.prototype);
+VignettePass.prototype.constructor = VignettePass;
+
+VignettePass.prototype.run = function(composer) {
+  this.shader.uniforms.boost.value = this.params.boost;
+  this.shader.uniforms.reduction.value = this.params.reduction;
+  composer.pass(this.shader);
+};
